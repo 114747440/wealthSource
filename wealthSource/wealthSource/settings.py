ff131b22f2bc7e01592c67e7bfe9462e78a76aeb
System Administrator <root@91-228-115-208.thinkdedicated.com>
2015-05-29 00:50:58 +0800

    initial version

---
+++ wealthSource/wealthSource/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for wealthSource project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'wealthSource'
+
+SPIDER_MODULES = ['wealthSource.spiders']
+NEWSPIDER_MODULE = 'wealthSource.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'wealthSource (+http://www.yourdomain.com)'
